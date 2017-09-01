3d233b644c8fca740cf3ccc9028d0055009ce65c
tomoya <ihatov08@gmail.com>
2017-09-01 22:13:04 +0900

    [mod]use eval

--- app/controllers/answers/checks_controller.rb
+++ app/controllers/answers/checks_controller.rb
@@ -1,18 +1,8 @@
 class Answers::ChecksController < ApplicationController
   def create
-    filename = "#{SecureRandom.hex(16)}.rb"
-    File.open(filename, 'w') do |file|
-      file.puts "p ar = #{params[:answer]}.to_a"
-    end
     begin
-      puts 'start'
-      p Exercise.find_by_sql('select * from exercises').to_s
-      logger.info Exercise.find_by_sql('select * from exercises').to_s
-      p `rails runner #{filename}`.chomp
-      logger.info `rails runner #{filename}`.chomp
-      puts 'end'
       sql = Exercise.find_by_sql('select * from exercises')
-      ar_sql = `rails runner #{filename}`.chomp
+      ar_sql = eval(prams[:answer])
       bool = (sql == ar_sql)
     rescue => e
       redirect_to answers_wrong_path(params[:exercise_id])
