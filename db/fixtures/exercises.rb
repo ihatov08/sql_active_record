f823f63dd3f63314d4800d9b49c5617a5434dcce
tomoya <ihatov08@gmail.com>
2017-09-01 17:51:56 +0900

    [mod]seed更新

--- db/fixtures/exercises.rb
+++ db/fixtures/exercises.rb
@@ -1,6 +1,6 @@
 Exercise.seed do |s|
   s.id = 1
   s.title = 'select * from'
-  s.body = 'select * from productsをactive recordで書いてみよう！'
+  s.body = 'select * from exercisesをactive recordで書いてみよう！'
   s.answer = 'Exercise.all'
 end
