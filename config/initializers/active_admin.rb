35325de7afb589650eeb4a89ffcd2374dbdaefc3
tomoya <ihatov08@gmail.com>
2017-09-08 16:20:06 +0900

    [mod]conflict active admin css

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -290,4 +290,6 @@ ActiveAdmin.setup do |config|
   # You can inherit it with own class and inject it for all resources
   #
   # config.order_clause = MyOrderClause
+  config.clear_stylesheets!
+  config.register_stylesheet 'admin/active_admin.css'
 end
