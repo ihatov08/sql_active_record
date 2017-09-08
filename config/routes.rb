831d5824ca3d99512665b6895eb87353c5cf601d
tomoya <ihatov08@gmail.com>
2017-09-08 17:38:32 +0900

    [add]topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
