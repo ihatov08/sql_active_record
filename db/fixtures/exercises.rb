d5ff92e8ffd907d5bc3b2d2f6d6c1664df86a7f6
tomoya <ihatov08@gmail.com>
2017-09-15 16:36:14 +0900

    [mod]seedをyml化する

--- db/fixtures/exercises.rb
+++ db/fixtures/exercises.rb
@@ -1,5 +1,8 @@
-Exercise.seed do |s|
-  s.id = 1
-  s.title = "SELECT \"exercises\".* FROM \"exercises\""
-  s.table_name = 'exercises'
+exercises = YAML.load_file("#{Rails.root}/db/yml/exercises.yml")
+exercises.each do |exercise|
+  Exercise.seed do |s|
+    s.id = exercise['id']
+    s.title = exercise['title']
+    s.table_name = exercise['table_name']
+  end
 end
